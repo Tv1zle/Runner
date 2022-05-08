47aaa345875f622f8747e4a05ee0291b165c6215
User <User@WIN-D6I7BGDFHRA>
2022-05-08 09:11:39 +0200

    Добавьте файлы проекта.

---
+++ HomeWork 040.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    double Distance;
+    std::cout << "Distance(KM) = ";
+    std::cin >> Distance;
+
+
+    double Speed;
+    std::cout << "Speed(KM/H) = ";
+    std::cin >> Speed;
+
+
+    double InHours = Distance / Speed;
+    double InMinutes = InHours * 60;
+    double InSeconds = InMinutes * 60;
+
+
+    std::cout <<"Human run in hours: " << InHours << "\n";
+    std::cout << "Human run in minutes: " << InMinutes << "\n";
+    std::cout << "Human run in seconds: " << InSeconds << "\n";
+
+}
+
